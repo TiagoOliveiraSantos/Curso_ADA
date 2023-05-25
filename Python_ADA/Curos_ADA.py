711d1047e1ba86f06f4aabb39c5530e10884016c
TiagoOliveiraSantos <tiago.os29@gmail.com>
2023-05-25 09:05:15 -0300

    Atualizando nome

---
+++ Python_ADA/Curos_ADA.py
@@ -0,0 +1,21 @@
+#Conseito de entrada e saida de dados
+
+print('Hello Wolrd') #Saida de dados
+input('Digite algo') #Entrada de dados
+
+
+#Variaveis, texto e comentarios
+
+#Os comentarios podem ser realizados usando "#" ou " ''' "(ápas simples 3 vezes no inicio e final)
+
+#Variaveis
+idade = 26 #Criando uma vaiavel
+print(idade)
+nome = 'Tiago' #O texto sempre deve ser digitado entre aspas
+
+'''
+Tipos de variáveis
+
+1.int: números inteiros, ou seja, numeros som a parte decimal: 0, 5, -1, 1000
+
+'''
